2606f01523789b92eb9b41e4f366ad1308c421a8
Lars van Vianen <lars@vianen.com>
2024-08-25 07:37:27 +0200

    Create AppHeader.vue

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,8 @@
+<template>
+    <header class="">
+        <h1 class="">
+
+        </h1>
+        <a href="https://github.com/stylescape/example-vue/" target="_blank" rel="noopener">View on GitHub</a>
+    </header>
+</template>
