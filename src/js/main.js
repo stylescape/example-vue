fe62bf76be5012052678c6ba3e7bf96f1f3c0977
Lars van Vianen <lars@vianen.com>
2024-08-25 07:37:34 +0200

    Create main.js

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+import { createApp } from "vue"
+import App from "./App.vue"
+
+// Import our custom CSS
+import "../scss/styles.scss"
+
+// Import only the Stylescape components we need
+
+
+
+createApp(App).mount("#app")
+
+// Create an example popover
