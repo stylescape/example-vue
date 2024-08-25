9884612f3434ad96fb256a2b9582df0f8b39eb1a
Lars van Vianen <lars@vianen.com>
2024-08-25 07:37:53 +0200

    Create AppGuides.vue

---
+++ src/components/AppGuides.vue
@@ -0,0 +1,32 @@
+<script setup>
+import GuideItem from "./AppGuideItem.vue"
+</script>
+
+<script>
+export default {
+    data() {
+        return {
+            resources: [
+                { 
+                    url: "https://scape.style",
+                    title: "Stylescape"
+                },
+
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <h2>Guides</h2>
+    <p>Read more detailed instructions and documentation on using or contributing to Stylescape.</p>
+
+    <ul class="">
+        <GuideItem
+        v-for="item in resources"
+        :href="item.url"
+        :text="item.title"
+        />
+    </ul>
+</template>
