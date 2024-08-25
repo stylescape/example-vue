a9b2d1284378f45ece77493294effc03a197551e
Lars van Vianen <lars@vianen.com>
2024-08-25 07:37:24 +0200

    Update AppFooter.vue

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,4 +1,4 @@
 <template>
-  <hr class="mt-5 mb-4">
-  <p class="text-muted">Created and open sourced by the Stylescape team. Licensed MIT.</p>
+    <hr class="">
+    <p class="">Created and open sourced by the Stylescape team. Licensed MIT.</p>
 </template>
